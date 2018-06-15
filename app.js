ddb89dc25a062c575fecf83fa1f9a9aba22cb48b
Ahmed Jafri <ajafri@Ahmeds-Mac-Pro.local>
2018-06-15 16:52:36 -0700

    Remove registry link and update marriage date

--- app.js
+++ app.js
@@ -1,78 +1,79 @@
-var combo   = require('combohandler'),
-    express = require('express'),
-    exphbs  = require('express-handlebars'),
-    state   = require('express-state'),
-    expose  = require('express-expose')
-    
-    config     = require('./config'),
-    helpers    = require('./lib/helpers'),
-    middleware = require('./middleware'),
-    routes     = require('./routes'),
+var combo = require("combohandler"),
+  express = require("express"),
+  exphbs = require("express-handlebars"),
+  state = require("express-state"),
+  expose = require("express-expose");
 
-    app = express();
+(config = require("./config")),
+  (helpers = require("./lib/helpers")),
+  (middleware = require("./middleware")),
+  (routes = require("./routes")),
+  (app = express());
 
 // -- Configure ----------------------------------------------------------------
 
-app.set('name', 'Zainab-Ahmed Walima');
-app.set('env', config.env);
-app.set('port', config.port);
-app.set('views', config.dirs.views);
-app.set('view engine', 'hbs');
-app.set('state namespace', 'YUI.Env.LE');
-app.enable('strict routing');
+app.set("name", "Zainab-Ahmed Walima");
+app.set("env", config.env);
+app.set("port", config.port);
+app.set("views", config.dirs.views);
+app.set("view engine", "hbs");
+app.set("state namespace", "YUI.Env.LE");
+app.enable("strict routing");
 
-app.engine('hbs', exphbs({
-    defaultLayout: 'main',
-    extname      : '.hbs',
-    helpers      : helpers,
-    layoutsDir   : config.dirs.layouts,
-    partialsDir  : config.dirs.partials
-}));
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: helpers,
+    layoutsDir: config.dirs.layouts,
+    partialsDir: config.dirs.partials
+  })
+);
 
 // -- Locals -------------------------------------------------------------------
 
-app.expose(config.yui.config, 'window.YUI_config');
+app.expose(config.yui.config, "window.YUI_config");
 
 app.locals({
-    title   : 'Zainab & Ahmed',
-    appTitle: 'Z&A Walima',
+  title: "Zainab & Ahmed",
+  appTitle: "Z&A Walima",
 
-    version    : config.version,
-    yui_version: config.yui.version,
+  version: config.version,
+  yui_version: config.yui.version,
 
-    nav: [
-        {id: 'walima',   url: '/walima/',   label: 'Walima'},
-        {id: 'logistics', url: '/logistics/', label: 'Logistics'},
-        {id: 'registry',  url: '/registry/',  label: 'Registry'},
-        {id: 'rsvp',      url: '/rsvp/',      label: 'RSVP'}
-    ],
+  nav: [
+    { id: "walima", url: "/walima/", label: "Walima" },
+    { id: "logistics", url: "/logistics/", label: "Logistics" },
+    { id: "rsvp", url: "/rsvp/", label: "RSVP" }
+  ],
 
-    subnav: {
-        logistics: [
-            {id: 'logistics', url: '/logistics/',         label: 'Logistics'},
-            {id: 'outings',   url: '/logistics/outings/', label: 'Outings'}
-        ]
-    },
+  subnav: {
+    logistics: [
+      { id: "logistics", url: "/logistics/", label: "Logistics" },
+      { id: "outings", url: "/logistics/outings/", label: "Outings" }
+    ]
+  },
 
-    yui_module: 'le-main',
+  yui_module: "le-main",
 
-    pictos : config.pictos,
-    typekit: config.typekit,
+  pictos: config.pictos,
+  typekit: config.typekit,
 
-    isDevelopment: config.isDevelopment,
-    isProduction : config.isProduction,
+  isDevelopment: config.isDevelopment,
+  isProduction: config.isProduction,
 
-    min: config.isProduction ? '-min' : ''
+  min: config.isProduction ? "-min" : ""
 });
 
 // -- Middleware ---------------------------------------------------------------
 
 if (config.isDevelopment) {
-    app.use(express.logger('tiny'));
+  app.use(express.logger("tiny"));
 }
 
 app.use(express.compress());
-app.use(express.favicon(config.dirs.pub + '/favicon.ico'));
+app.use(express.favicon(config.dirs.pub + "/favicon.ico"));
 app.use(express.cookieParser());
 app.use(express.cookieSession(config.session));
 app.use(express.json());
@@ -81,7 +82,7 @@ app.use(express.methodOverride());
 app.use(express.csrf());
 app.use(middleware.csrfToken);
 app.use(middleware.invitation);
-app.use(middleware.pjax('bare'));
+app.use(middleware.pjax("bare"));
 app.use(middleware.checkDate);
 app.use(app.router);
 app.use(middleware.slash());
@@ -89,46 +90,48 @@ app.use(express.static(config.dirs.pub));
 app.use(middleware.notfound);
 
 if (config.isDevelopment) {
-    app.use(express.errorHandler({
-        dumpExceptions: true,
-        showStack     : true
-    }));
+  app.use(
+    express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    })
+  );
 } else {
-    app.use(middleware.error);
+  app.use(middleware.error);
 }
 
 // -- Routes -------------------------------------------------------------------
 
-app.get('/', routes.render('home'));
+app.get("/", routes.render("home"));
 
-app.get('/walima/', routes.render('walima'));
+app.get("/walima/", routes.render("walima"));
 
-app.get('/logistics/',         routes.render('logistics'));
-app.get('/logistics/hotels/',  routes.render('logistics/hotels'));
-app.get('/logistics/outings/', routes.render('logistics/outings'));
+app.get("/logistics/", routes.render("logistics"));
+app.get("/logistics/hotels/", routes.render("logistics/hotels"));
+app.get("/logistics/outings/", routes.render("logistics/outings"));
 
-app.get('/registry/', routes.render('registry'));
+//app.get('/registry/', routes.render('registry'));
 
-app.get( '/rsvp/',                       routes.rsvp.pub, routes.rsvp.edit);
-app.post('/rsvp/',                       routes.rsvp.resend);
-app.get( '/rsvp/brunch/',                routes.rsvp.brunch);
-app.post('/rsvp/brunch/',                routes.rsvp.brunch);
-app.get( '/rsvp/brunch/:invitation_key', routes.rsvp.login);
-app.get( '/rsvp/:invitation_key',        routes.rsvp.login);
+app.get("/rsvp/", routes.rsvp.pub, routes.rsvp.edit);
+app.post("/rsvp/", routes.rsvp.resend);
+app.get("/rsvp/brunch/", routes.rsvp.brunch);
+app.post("/rsvp/brunch/", routes.rsvp.brunch);
+app.get("/rsvp/brunch/:invitation_key", routes.rsvp.login);
+app.get("/rsvp/:invitation_key", routes.rsvp.login);
 
-app.all( '/invitations/:invitation/*',       middleware.auth.ensureInvitation);
-app.get( '/invitations/:invitation/',        routes.invitations.read);
-app.put( '/invitations/:invitation/',        routes.invitations.update);
-app.get( '/invitations/:invitation/guests',  routes.invitations.readGuests);
-app.post('/invitations/:invitation/confirm', routes.invitations.confirm);
+app.all("/invitations/:invitation/*", middleware.auth.ensureInvitation);
+app.get("/invitations/:invitation/", routes.invitations.read);
+app.put("/invitations/:invitation/", routes.invitations.update);
+app.get("/invitations/:invitation/guests", routes.invitations.readGuests);
+app.post("/invitations/:invitation/confirm", routes.invitations.confirm);
 
-app.all('/guests/:guest/', middleware.auth.ensureGuest);
-app.get('/guests/:guest/', routes.guests.read);
-app.put('/guests/:guest/', routes.guests.update);
+app.all("/guests/:guest/", middleware.auth.ensureGuest);
+app.get("/guests/:guest/", routes.guests.read);
+app.put("/guests/:guest/", routes.guests.update);
 
-app.get('/combo/:version', [
-    combo.combine({rootPath: config.dirs.pub}),
-    combo.respond
+app.get("/combo/:version", [
+  combo.combine({ rootPath: config.dirs.pub }),
+  combo.respond
 ]);
 
 module.exports = app;
